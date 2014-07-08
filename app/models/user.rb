75bdefa5a74a5e6b2acaf69d13669561d8831bd6
Marcin exine Merda <marcin@exine.eu.org>
2014-07-08 23:51:47 +0200

    user: add firstname and lastname

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :products
 
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+
   def admin?
   	self[:admin]
   end
