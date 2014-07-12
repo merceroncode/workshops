ddb71d12c518a94ff15a4d617a34d5a040ce6f39
Marcin exine Merda <marcin@exine.eu.org>
2014-07-12 15:33:37 +0200

    product: add validations and average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+  	rev = self.reviews.all
+  	rating = 0.0
+  	rev.each do |r|
+  		rating = rating + r.rating
+  	end
+
+  	rating / rev.count
+  end
 end
